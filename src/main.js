7ca99e1c70e955cabb965bb81638183656eedf74
Hashane <jhashane@gmail.com>
2025-08-28 07:31:30 +0300

    Added: Apollo

--- src/main.js
+++ src/main.js
@@ -3,4 +3,25 @@ import router from './routes'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+    uri: 'http://bloggraphql.test/graphiql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+})
+
+const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient,
+})
+
+createApp(App).use(router).use(apolloProvider).mount('#app')
